d8754846a87eabfc59964bb2260efd09af111c74
Hoàng Tú <50492205+hoangtularva@users.noreply.github.com>
2020-06-28 17:49:04 +0700

    home page

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,11 @@
+class StoreController < ApplicationController
+  
+  include CurrentCart
+  before_action :set_cart
+  
+  def index
+    @products = Product.order(:title)
+    #  @products = Product.all
+
+  end
+end
