a7c6d03d158720ad23353076828ff16bd8fa24c6
hoangtularva <hoangtukg295@gmail.com>
2020-06-29 17:10:47 +0700

    Broadcasting Updates with Action Cable

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #Action cable
+  config.action_cable.disable_request_forgery_protection = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
