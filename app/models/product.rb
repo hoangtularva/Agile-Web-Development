feb20d9bb8940a77948c95d940463dfd0c2fb760
hoangtularva <hoangtukg295@gmail.com>
2020-06-29 15:28:10 +0700

    relationship model

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,16 @@ class Product < ApplicationRecord
         message: 'must be a URL for GIF, JPG or PNG image.'
     }
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
+    private
+        # ensure that there are no line items referencing this product
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items present')
+                throw :abort
+            end
+        end
 end
