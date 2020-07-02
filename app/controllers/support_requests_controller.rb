105e5337d4d8a6024aa3f8f78d697ac8769fd0a0
hoangtularva <hoangtukg295@gmail.com>
2020-07-02 09:05:16 +0700

    support request mailer respond

--- app/controllers/support_requests_controller.rb
+++ app/controllers/support_requests_controller.rb
@@ -2,4 +2,11 @@ class SupportRequestsController < ApplicationController
     def index
         @support_requests = SupportRequest.all
     end
+
+    def update
+        support_request = SupportRequest.find(params[:id])
+        support_request.update(response: params.require(:support_request)[:response])
+        SupportRequestMailer.respond(support_request).deliver_now
+        redirect_to support_requests_path
+    end
 end
