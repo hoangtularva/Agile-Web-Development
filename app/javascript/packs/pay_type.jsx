83d97187e9e9e1eefb496b976988ef9d07b587bd
hoangtularva <hoangtukg295@gmail.com>
2020-07-06 22:22:13 +0700

    add React PayTypeSelector

---
+++ app/javascript/packs/pay_type.jsx
@@ -0,0 +1,9 @@
+import React
+from 'react'
+import ReactDOM
+from 'react-dom'
+import PayTypeSelector from 'PayTypeSelector'
+document.addEventListener('turbolinks:load', function() {
+var element = document.getElementById("pay-type-component");
+ReactDOM.render(<PayTypeSelector />, element);
+});
