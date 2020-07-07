752f4544dbe6687602749eb107da3bfa509da3dc
hoangtularva <hoangtukg295@gmail.com>
2020-07-08 00:36:16 +0700

    update layouts and mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,7 +36,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         ChargeOrderJob.perform_later(@order,pay_type_params.to_h)
-        OrderMailer.received(@order).deliver_later
+        OrderMailer.received(@order).deliver
         format.html { redirect_to store_index_url(locale: I18n.locale), notice: I18n.t('.thanks')}
         format.json { render :show, status: :created, location: @order }
       else
